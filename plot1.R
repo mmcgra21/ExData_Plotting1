1bd57a5423a205293be648a1b76ceb5f3e17146d
mmcgra21 <mwm2166@columbia.edu>
2021-07-28 16:40:23 -0400

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,30 @@
+## Read in data txt file as table (it will read in the entire dataset
+## (2,075,259 rows and 9 columns), so ensure you have enough memory space).
+data <- read.table("household_power_consumption.txt",
+                   header=TRUE,
+                   sep=";",
+                   na.strings = "?")
+
+## Subset the entire dataset to just the rows that have the dates 
+## 2007-02-01 and 2007-02-02.
+data <- subset(data, Date %in% c("1/2/2007", "2/2/2007"))
+## Reset the row indices.
+rownames(data) <- NULL
+
+## Convert the "Global_active_power" column to numeric.
+data$Global_active_power <- as.numeric(data$Global_active_power)
+
+## Create png file "plot1.png"
+png(filename = "plot1.png",
+    width = 480,
+    height = 480)
+
+## Create the Global Active Power histogram.
+hist(data$Global_active_power,
+     col="red",
+     xlab="Global Active Power (kilowatts)",
+     ylab="Frequency",
+     main="Global Active Power")
+
+## Close png file.
+dev.off()
